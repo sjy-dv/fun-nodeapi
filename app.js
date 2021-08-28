685d6e427b0e1abc8f770697ecb0912b9ee49509
sjy <ks2414e@naver.com>
2021-08-29 03:12:00 +0900

    ver111

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+//using for api
 hello = (req, res) => {
   res.send("hello");
 };
@@ -15,5 +16,18 @@ let api = [hello, bye];
 for (let i = 0; i < 2; i++) {
   app.get(url[i], api[i]);
 }
+
+// using multi
+app
+  .get("/", (req, res) => {
+    res.send("hello");
+  })
+  .get("/1", (req, res) => {
+    res.send("1");
+  });
+
+//using router && controller
+app.get("/2", hello);
+app.get("/3", bye);
 
 app.listen(8081);
